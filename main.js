f702100e3730d8b2f66b9f2df9a438c37ecb65eb
person-cloud <79553402+person-cloud@users.noreply.github.com>
2021-12-07 21:50:49 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function preload() 
+{ } 
+
+function setup() 
+{ canvas = createCanvas(300,300); canvas.center(); } 
+
+function draw() 
+{ } 
+
+function takeSnapshot() { save('u_a_clown.png'); }
